b0f4f98f857fbd8224a2784aaa9b24973b440b4d
Thomas <trasmussen@redventures.com>
2020-01-29 18:25:25 -0500

    racers section preeeeetttttttty much done

--- components/Racers.jsx
+++ components/Racers.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import Section from './shared/Section';
 import RacerCard from './RacerCard';
+import { users } from '../data/racers.json';
 
 const RacerContainer = styled.div`
   width: 100%;
@@ -10,41 +11,18 @@ const RacerContainer = styled.div`
   flex-flow: row wrap;
 `;
 
-/*
- * const racers = [
- * {
- *  username: "tallestthomas",
- *  socials: {
- *    twitch: String,
- *    youtube: String,
- *    twitter: String,
- *    instagram: String
- *  }
- * }
- * ];
- *
- *
- */
-
-const tallestthomas = {
-  username: 'TallestThomas',
-  socials: {
-    twitch: 'tallestthomas',
-    twitter: 'tallestthomas',
-    instagram: 'tallestthomas',
-  },
-  profileImage: 'https://placekitten.com/50/50',
-};
 
 export default () => (
   <Section title="Racers">
     <RacerContainer>
-      <RacerCard username={tallestthomas.username} socials={tallestthomas.socials} profileImage={tallestthomas.profileImage} />
-      <RacerCard username="PointCrow" />
-      <RacerCard username="TallestThomas" />
-      <RacerCard username="PointCrow" />
-      <RacerCard username="TallestThomas" />
-      <RacerCard username="PointCrow" />
+      { users.map((user) => (
+        <RacerCard
+          key={user.username}
+          username={user.username}
+          socials={user.socials}
+          profileImage={user.profileImage}
+        />
+      ))}
     </RacerContainer>
   </Section>
 );
